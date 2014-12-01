c39ad80a73be5f938d60efbf907f476a5d9c6875
Max Kalachev <maxkalachev@yandex.ru>
2014-12-01 23:54:32 +0400

    cool notation for action's call

--- test/files.js
+++ test/files.js
@@ -1,7 +1,12 @@
 
 
-fm.script([
-  fm.act(act.fs.files.concat, {sources: ['file1.txt', 'file2.txt', 'file3.txt'], target: 'output.txt'})
-])(function() {
+var script = fm.script([
+  act.fs.concat, ['file1.txt', 'file2.txt', 'file3.txt'], 'output.txt'
+]);
+
+
+script(handleSuccess, console.error);
+
+function handleSuccess() {
   console.log('Operation successfully completed.');
-}, console.error);
+}
