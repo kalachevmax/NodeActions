c845fff37d964e838065daec6dd0b0eb7bda48a3
Maxim Kalachev <maxkalachev@yandex.ru>
2014-12-04 15:26:42 +0300

    type constructors

--- test/files.js
+++ test/files.js
@@ -1,17 +1,17 @@
 
 
 var script = fm.script([
-  fm.LIST_STRING(fm.script([
+  fm.LIST_STRING([
     fm.STRING('Enter sources: '),
     act.cli.put,
     act.cli.get
-  ])),
+  ]),
 
-  fm.STRING('target', fm.script([
+  fm.STRING([
     fm.STRING('Enter target: '),
     act.cli.put,
     act.cli.get
-  ])),
+  ]),
 
   act.fs.concat
 ]);
